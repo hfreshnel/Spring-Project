0b10c2c212ce30106f067874e5b7a1d96812024d
vincel57 <vince.lwembet@groupe-esigelec.org>
2024-12-09 17:19:53 +0100

    Classe QuestionService

---
+++ src/main/java/com/isn/quizplatform/service/QuestionService.java
@@ -0,0 +1,49 @@
+package com.isn.quizplatform.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.isn.quizplatform.model.Question;
+import com.isn.quizplatform.repository.QuestionRepository;
+
+@Service
+public class QuestionService {
+
+	@Autowired
+    private QuestionRepository questionimpl;
+	
+	public QuestionService() {
+	}
+    public List<Question> getAllQuestionsByQuizId(Long quizId) {
+        return questionimpl.findAll(); 
+    }
+
+    public Optional<Question> getQuestionById(Long questionId) {
+        return questionimpl.findById(questionId);
+    }
+
+    public Question createQuestion(Question question) {
+        return questionimpl.save(question);
+    }
+
+    public Optional<Question> updateQuestion(Long questionId, Question updatedQuestion) {
+        return questionimpl.findById(questionId).map(existingQuestion -> {
+          
+            return questionimpl.save(existingQuestion);
+        });
+    }
+
+    public boolean deleteQuestion(Long questionId) {
+        if (questionimpl.existsById(questionId)) {
+        	questionimpl.deleteById(questionId);
+            return true;
+        }
+        return false;
+    }
+	
+	
+
+}
