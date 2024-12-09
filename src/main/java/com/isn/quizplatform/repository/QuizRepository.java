2a8d5b103a9866c0a9c715d88904410446d07e78
mor <mor.talla@groupe-esigelec.org>
2024-12-09 15:29:35 +0100

    Revert "Adding the interface QuizRepository"
    This reverts commit fbe8a760bf83135a746b920108715abc42a08221.

--- src/main/java/com/isn/quizplatform/repository/QuizRepository.java
+++
@@ -1,7 +0,0 @@
-package com.isn.quizplatform.repository;
-
-import com.isn.quizplatform.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-}
