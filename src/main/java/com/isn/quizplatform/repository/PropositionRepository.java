cb206f7cb8a85751494a446e60a39ed8e470385c
Celsin <cmcelsin@gmail.com>
2024-12-09 16:58:35 +0100

    add proposition v0

---
+++ src/main/java/com/isn/quizplatform/repository/PropositionRepository.java
@@ -0,0 +1,9 @@
+package com.isn.quizplatform.repository;
+
+import com.isn.quizplatform.model.Proposition;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropositionRepository extends JpaRepository<Proposition, Long> {
+
+}
