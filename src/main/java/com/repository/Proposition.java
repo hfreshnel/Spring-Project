39fd6459d9869a52c634c4f0f31e91b0ae2680b4
BriceProjet <brice.clain@groupe-esigelec.org>
2024-12-09 12:27:36 +0100

    refactor: imports oubliés

--- src/main/java/com/repository/Proposition.java
+++ src/main/java/com/repository/Proposition.java
@@ -1,5 +1,10 @@
 package com.repository;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 @Entity
 public class Proposition {
     @Id
