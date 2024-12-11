d75abe505a4723f811b9c25ed13c9f39c8571de7
PierreJeanLievens <pierre-jean.lievens@groupe-esigelec.org>
2024-12-11 12:17:24 +0100

    add changes for h2

--- src/test/java/com/isn/quizplatform/repository/PersonneRepositoryTests.java
+++ src/test/java/com/isn/quizplatform/repository/PersonneRepositoryTests.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(classes = Application.class)
+@ActiveProfiles("test")
+@DataJpaTest
 public class PersonneRepositoryTests {
 
     @Autowired
