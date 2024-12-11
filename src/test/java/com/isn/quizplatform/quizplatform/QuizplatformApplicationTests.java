857bdcea357ff3b1ae66c054ef19aa0e3eb0b8b7
PierreJeanLievens <pierre-jean.lievens@groupe-esigelec.org>
2024-12-11 10:43:39 +0100

    refactor quiplatformTest class

--- src/test/java/com/isn/quizplatform/quizplatform/QuizplatformApplicationTests.java
+++ src/test/java/com/isn/quizplatform/quizplatform/QuizplatformApplicationTests.java
@@ -3,7 +3,9 @@ package com.isn.quizplatform.quizplatform;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.isn.quizplatform.application.Application;
+
+@SpringBootTest(classes = Application.class)
 class QuizplatformApplicationTests {
 
 	@Test
