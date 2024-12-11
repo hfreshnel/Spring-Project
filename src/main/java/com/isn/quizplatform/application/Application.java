ce26367d2b7ad84e92e7cee34af3202441e4030f
PierreJeanLievens <pierre-jean.lievens@groupe-esigelec.org>
2024-12-11 09:25:57 +0100

    changement classe PersonneTestRepository

--- src/main/java/com/isn/quizplatform/application/Application.java
+++ src/main/java/com/isn/quizplatform/application/Application.java
@@ -1,31 +1,25 @@
 package com.isn.quizplatform.application;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.ApplicationContext;
-
-import com.isn.quizplatform.model.Personne;
-import com.isn.quizplatform.repository.PersonneRepository;
-
-@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-			ApplicationContext context = SpringApplication.run(Application.class, args);
+			// ApplicationContext context = SpringApplication.run(Application.class, args);
 
-			// Obtention du bean PersonneRepository
-			PersonneRepository personneRepository = context.getBean(PersonneRepository.class);
+			// // Obtention du bean PersonneRepository
+			// @Autowired
+			// PersonneRepository personneRepository;
 
-			// Création d'une personne
-			Personne personne = new Personne("Math", "Anderson", "test@mail", "test", 0);
+			// // // Création d'une personne
+			// // Personne personne = new Personne("Math", "Anderson", "test@mail", "test", 0);
 	
-			// Sauvegarde dans la base de données
-			personneRepository.save(personne);
+			// // Sauvegarde dans la base de données
+			// personneRepository.getById("1");
 	
-			// Vérification de l'enregistrement
-			System.out.println("Personne sauvegardée : " + personne);
+			// // Vérification de l'enregistrement
+			// System.out.println("Personne sauvegardée : " + personne);
 	}
 
 }
