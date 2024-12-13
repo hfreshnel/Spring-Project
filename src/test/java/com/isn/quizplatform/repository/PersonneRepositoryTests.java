a1c88a3194f25580dd018d333cecbbecaf2ee941
Tenefou <gaspar.rg69@gmail.com>
2024-12-13 09:22:22 +0100

    Finalisation tests et fin de mise en place de h2

--- src/test/java/com/isn/quizplatform/repository/PersonneRepositoryTests.java
+++ src/test/java/com/isn/quizplatform/repository/PersonneRepositoryTests.java
@@ -17,17 +17,43 @@ public class PersonneRepositoryTests {
     private PersonneRepository personneRepository;
 
     @Test
-    public void testSavePersonne() {
+    public void testSaveAndFindByIdPersonne() {
+        // Given
+        Personne personne = new Personne("Dupont", "Jean", "jean.dupont@example.com", "password123", 1);
+        
+        // When
+        Personne savedPersonne = personneRepository.save(personne);
+
+        Personne foundPersonne = personneRepository.findById(savedPersonne.getId()).orElse(null);
+
+        // Then
+        assertThat(foundPersonne).isNotNull();
+        assertThat(foundPersonne.getId()).isNotNull();
+        assertThat(foundPersonne.getNom()).isEqualTo("Dupont");
+        assertThat(foundPersonne.getPrenom()).isEqualTo("Jean");
+        assertThat(foundPersonne.getMail()).isEqualTo("jean.dupont@example.com");
+        assertThat(foundPersonne.getMdp()).isEqualTo("password123");
+        assertThat(foundPersonne.getRole()).isEqualTo(1);
+    }
+
+    @Test
+    public void testFindByMailPersonne() {
         // Given
         Personne personne = new Personne("Dupont", "Jean", "jean.dupont@example.com", "password123", 1);
 
         // When
         Personne savedPersonne = personneRepository.save(personne);
 
+        Personne foundPersonne = personneRepository.findByMail(savedPersonne.getMail()).orElse(null);
+
         // Then
-        assertThat(savedPersonne).isNotNull();
-        assertThat(savedPersonne.getId()).isNotNull();
-        assertThat(savedPersonne.getNom()).isEqualTo("Dupont");
-        assertThat(savedPersonne.getPrenom()).isEqualTo("Jean");
+        assertThat(foundPersonne).isNotNull();
+        assertThat(foundPersonne.getId()).isNotNull();
+        assertThat(foundPersonne.getNom()).isEqualTo("Dupont");
+        assertThat(foundPersonne.getPrenom()).isEqualTo("Jean");
+        assertThat(foundPersonne.getMail()).isEqualTo("jean.dupont@example.com");
+        assertThat(foundPersonne.getMdp()).isEqualTo("password123");
+        assertThat(foundPersonne.getRole()).isEqualTo(1);
     }
 }
+
