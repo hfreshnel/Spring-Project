00470746774faffd2efe009b4a501e869fc21ec6
mor <mor.talla@groupe-esigelec.org>
2024-12-13 11:50:10 +0100

    Adding modifications to securityConfig

--- src/main/java/com/isn/quizplatform/security/SecurityConfig.java
+++ src/main/java/com/isn/quizplatform/security/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/auth/register", "/public/auth/login","/swagger-ui/**", "/v3/api-docs/**")
+                        .requestMatchers("/public/auth/register", "/public/auth/login","/swagger-ui/**", "/v3/api-docs/**", "/public/quiz/**")
                         .permitAll()
                         .anyRequest().authenticated()
                 )
