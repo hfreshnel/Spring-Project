5cb80813899138347a67eb0dea74aa18b34ce211
Tenefou <gaspar.rg69@gmail.com>
2024-12-13 10:04:13 +0100

    Amélioration tests et implémentation test delete()

--- src/test/java/com/isn/quizplatform/repository/PersonneRepositoryTests.java
+++ src/test/java/com/isn/quizplatform/repository/PersonneRepositoryTests.java
@@ -1,5 +1,6 @@
 package com.isn.quizplatform.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,14 +17,43 @@ public class PersonneRepositoryTests {
     @Autowired
     private PersonneRepository personneRepository;
 
-    @Test
-    public void testSaveAndFindByIdPersonne() {
-        // Given
-        Personne personne = new Personne("Dupont", "Jean", "jean.dupont@example.com", "password123", 1);
-        
-        // When
-        Personne savedPersonne = personneRepository.save(personne);
+    private Personne defaultPersonne1;
+    private Personne defaultPersonne2;
+    private Personne savedPersonne;
 
+    @BeforeEach
+    public void setup() {
+        personneRepository.deleteAll(); // Nettoie la table avant chaque test
+        defaultPersonne1 = new Personne("Dupont", "Jean", "jean.dupont@example.com", "password123", 1);
+        defaultPersonne2 = new Personne("john", "Jones", "john.jones@example.com", "1234", 1000);
+
+        savedPersonne = personneRepository.save(defaultPersonne1);
+    }
+
+
+    @Test
+    public void testSaveFindByIdAndFindByMailPersonne() {
+        // Given
+        // When
+        Personne foundPersonne1 = personneRepository.findByMail(defaultPersonne1.getMail()).orElse(null);
+        Personne foundPersonne2 = personneRepository.findByMail(defaultPersonne2.getMail()).orElse(null);
+
+
+        // Then
+        assertThat(foundPersonne2).isNull();
+        assertThat(foundPersonne1).isNotNull();
+        assertThat(foundPersonne1.getId()).isNotNull();
+        assertThat(foundPersonne1.getNom()).isEqualTo("Dupont");
+        assertThat(foundPersonne1.getPrenom()).isEqualTo("Jean");
+        assertThat(foundPersonne1.getMail()).isEqualTo("jean.dupont@example.com");
+        assertThat(foundPersonne1.getMdp()).isEqualTo("password123");
+        assertThat(foundPersonne1.getRole()).isEqualTo(1);
+    }
+
+    @Test
+    public void testFindByIdPersonne() {
+        // Given
+        // When
         Personne foundPersonne = personneRepository.findById(savedPersonne.getId()).orElse(null);
 
         // Then
@@ -37,23 +67,13 @@ public class PersonneRepositoryTests {
     }
 
     @Test
-    public void testFindByMailPersonne() {
+    public void testDelete() {
         // Given
-        Personne personne = new Personne("Dupont", "Jean", "jean.dupont@example.com", "password123", 1);
-
         // When
-        Personne savedPersonne = personneRepository.save(personne);
-
-        Personne foundPersonne = personneRepository.findByMail(savedPersonne.getMail()).orElse(null);
+        personneRepository.delete(defaultPersonne1);
+        Personne foundPersonne = personneRepository.findById(savedPersonne.getId()).orElse(null);
 
         // Then
-        assertThat(foundPersonne).isNotNull();
-        assertThat(foundPersonne.getId()).isNotNull();
-        assertThat(foundPersonne.getNom()).isEqualTo("Dupont");
-        assertThat(foundPersonne.getPrenom()).isEqualTo("Jean");
-        assertThat(foundPersonne.getMail()).isEqualTo("jean.dupont@example.com");
-        assertThat(foundPersonne.getMdp()).isEqualTo("password123");
-        assertThat(foundPersonne.getRole()).isEqualTo(1);
+        assertThat(foundPersonne).isNull();
     }
 }
-
