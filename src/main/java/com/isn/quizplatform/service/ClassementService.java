1e9b1eafc6b8a4ba1d0870da58a798596894acbb
PierreJeanLievens <pierre-jean.lievens@groupe-esigelec.org>
2024-12-18 09:53:52 +0100

    add Service&Controller to call the procedure

---
+++ src/main/java/com/isn/quizplatform/service/ClassementService.java
@@ -0,0 +1,28 @@
+package com.isn.quizplatform.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ClassementService {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<Map<String, Object>> getClassementByQuizId(Long quizID) {
+        // Appel de la procédure stockée
+        String sql = "CALL CalculeClassementParQuiz(?)";
+        return jdbcTemplate.queryForList(sql, quizID);
+    }
+    
+    public List<Map<String, Object>> getPropositionsPercentage(Long questionID) {
+        // Appel de la procédure stockée
+        System.out.println("Calling GetPropositionsPercentage with questionID: " + questionID);
+        String sql = "CALL GetPropositionsPercentage(?)";
+        return jdbcTemplate.queryForList(sql, questionID);
+    }
+}
