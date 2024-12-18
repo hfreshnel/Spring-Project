fc9029adafbe5ef9738bdd702a69d55d7099df7d
FU Qianchen <bhaaer12134@gmail.com>
2024-12-18 10:29:54 +0100

    Add ApiResponse into login and connection

--- src/main/java/com/isn/quizplatform/service/AuthService.java
+++ src/main/java/com/isn/quizplatform/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.isn.quizplatform.service;
 
+import com.isn.quizplatform.model.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,38 +15,45 @@ public class AuthService {
 
 	private PersonneRepository PR;
 	private BCryptPasswordEncoder passwordEncoder;
+
 	
     public AuthService(PersonneRepository PR, BCryptPasswordEncoder passwordEncoder) {
         this.PR = PR;
         this.passwordEncoder = passwordEncoder;
     }
 	
-	public void register(Personne personne) {
-		PR.findByMail(personne.getMail())
-		.ifPresent(existingPersonne -> {
-			throw new RuntimeException("Un utilisateur avec cet email existe déjà");
-		});
-		
-		String hashedPassword = passwordEncoder.encode(personne.getMdp());
-        personne.setMdp(hashedPassword);
+	public ApiResponse<Personne> register(Personne personne) {
+			if(PR.findByMail(personne.getMail()).isPresent()){
+				return new ApiResponse<>(null, 400, "The user already exists");
+			}
+			String hashedPassword = passwordEncoder.encode(personne.getMdp());
+			personne.setMdp(hashedPassword);
 
-		PR.save(personne);
-		}
-	
-	public Personne login(String email, String password) {
-		Personne personne = PR.findByMail(email)
-                .orElseThrow(() -> new RuntimeException("E-mail ou mot de passe invalide"));
-
-        if (!passwordEncoder.matches(password, personne.getMdp())) {
-            throw new RuntimeException("E-mail ou mot de passe invalide");
-        }
-
-		if (personne.getRole() == 0) {
 			personne.setRole(0);
+		try {
+			PR.save(personne);
+			return new ApiResponse<>(personne, 201, null); // Succès, utilisateur créé
+		} catch (Exception e) {
+			return new ApiResponse<>(null, 500, "registration failed"); // Erreur interne
 		}
+	}
+	
+	public ApiResponse<Personne> login(String email, String password) {
+		try {
+			Personne personne = PR.findByMail(email)
+					.orElseThrow(() -> new RuntimeException("E-mail ou mot de passe invalide"));
 
-        System.out.println("Utilisateur connecté avec succès : " + personne.getMail());
-		return personne;
+			if (!passwordEncoder.matches(password, personne.getMdp())) {
+				throw new RuntimeException("E-mail ou mot de passe invalide");
+			}
+
+			System.out.println("Utilisateur connecté avec succès : " + personne.getMail());
+			return new ApiResponse<>(personne, 201, null);
+		} catch (RuntimeException e){
+			return new ApiResponse<>(null, 400, "E-mail ou mot de passe invalide");
+		} catch (Exception e){
+			return new ApiResponse<>(null, 500, "Login.failed");
+		}
 	}
 
 }
