081aa3873ee7cf0c6976f6c61ca2ea15b7853e1d
Marc CHABI <marc.chabi@groupe-esigelec.org>
2025-01-10 15:02:52 +0100

    Update ChoisirController.java

--- src/main/java/com/isn/quizplatform/controller/ChoisirController.java
+++ src/main/java/com/isn/quizplatform/controller/ChoisirController.java
@@ -1,40 +1,36 @@
 package com.isn.quizplatform.controller;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+ 
 import com.isn.quizplatform.model.ApiResponse;
 import com.isn.quizplatform.service.ChoisirService;
-
+ 
 import java.util.List;
 import java.util.Map;
-
+ 
 @CrossOrigin(origins = "*")
 @RestController
 public class ChoisirController {
-
+ 
 	@Autowired
 	private ChoisirService chosirService;
-	
 	@GetMapping("/public/quiz/{quiz_id}/classement")
-    public ApiResponse<List<Map<String, Object>>> getClassement(@RequestParam("quiz_id") Long quizID) {
-        return new ApiResponse<List<Map<String, Object>>>(chosirService.getClassementByQuizId(quizID),200,null);
+    public ApiResponse<List<Map<String, Object>>> getClassement(@PathVariable Long quiz_id) {
+        return new ApiResponse<List<Map<String, Object>>>(chosirService.getClassementByQuizId(quiz_id),200,null);
     }
-	
 	 @GetMapping("/public/quiz/{quiz_id}/questions/{question_id}/")
-	    public ApiResponse<List<Map<String, Object>>> getPropositionsPercentage(@RequestParam("question_id") Long questionID,@RequestParam("quizID") Long quizID) {
-	        System.out.println("Controller: Received request for /propositions-percentage with questionID: " + questionID + " and quizID: " + quizID);
-	    	
-	    	return new ApiResponse<List<Map<String, Object>>>(chosirService.getPropositionsPercentage(questionID,quizID),200,null);
+	    public ApiResponse<List<Map<String, Object>>> getPropositionsPercentage(@PathVariable Long question_id,@PathVariable Long quiz_id) {
+	        System.out.println("Controller: Received request for /propositions-percentage with questionID: " + question_id + " and quizID: " + quiz_id);
+	    	return new ApiResponse<List<Map<String, Object>>>(chosirService.getPropositionsPercentage(question_id,quiz_id),200,null);
 	 }
-	 
 	 @GetMapping("/public/quiz/{quiz_id}/proposition/{proposition_id}/user/{user_id}")
-	    public ApiResponse<String> selectPro(@RequestParam("quizID") Long quizID,@RequestParam("proposition_id") Long proposition_id,@RequestParam("user_id") Long user_id) {
-	        System.out.println("Controller: Received request for /propositions-percentage with questionID: " + proposition_id + " and quizID: " + quizID);
-	    	
-	    	return new ApiResponse<String>(chosirService.addProposition(quizID,proposition_id,user_id),200,null);
+	    public ApiResponse<String> selectPro(@PathVariable Long quiz_id,@PathVariable Long proposition_id,@PathVariable Long user_id) {
+	        System.out.println("Controller: Received request for /propositions-percentage with questionID: " + proposition_id + " and quizID: " + quiz_id);
+	    	return new ApiResponse<String>(chosirService.addProposition(quiz_id,proposition_id,user_id),200,null);
 	 }
 }
